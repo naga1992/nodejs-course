54f2c77fabb2a594b27f928186d8032c9b0aea05
naga suresh <nagasuresh@nagas-MacBook-Pro.local>
2020-06-07 09:11:02 +0530

    sec-3ls9

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,1 +1,7 @@
-requestAnimationFrame('fs')
+const fs = require('fs')
+// this command creats a file if it does not exist
+fs.writeFileSync('notes.txt','this file is created by nodejs')
+
+// challenge : Append a message to notes.txt
+
+fs.appendFileSync('notes.txt','this is my first append message')
