a2aa244326446f11d33a63a5c5facbf5fb51942a
naga suresh <nagasuresh@nagas-MacBook-Pro.local>
2020-06-07 09:23:59 +0530

    sec3lec10pt1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,2 @@
-const fs = require('fs')
-// this command creats a file if it does not exist
-fs.writeFileSync('notes.txt','this file is created by nodejs')
-
-// challenge : Append a message to notes.txt
-
-fs.appendFileSync('notes.txt','this is my first append message')
+const name = require('./utils')
+console.log(name)
