0ba5a8ffdb83b0c7576caf65ccc07311fe4186c2
naga suresh <nagasuresh@nagas-MacBook-Pro.local>
2020-06-07 15:00:58 +0530

    try validator package

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,5 +2,9 @@
 // const sum= add(4,-2)
 // console.log(sum)
 
+const  validator=require('validator')
 const getNotes = require('./notes')
+
 console.log(getNotes())
+
+console.log(validator.isEmail("abc@test.com"))
